d3db5e5e9e264a0c24be5c8cbaa747f9692a2948
linpinghua <linpinghua@kingsoft.com>
2022-08-15 09:17:32 +0800

    第一次提交ts基础

--- src/01-ts基础/06-函数式组件.tsx
+++ src/01-ts基础/06-函数式组件.tsx
@@ -1,29 +1,30 @@
-import React, { useState } from 'react'
+import React, { useState } from "react"
 // 第一种使用泛型<>,不写，函数组件也会提示
 //const [name, setname] = useState<string>('hua')
 export default function App() {
-  const [name, setname] = useState<string>('hua')
-  return (
-    <div>
-      App--{name}
-      <button
-        onClick={() => {
-          setname('海绵宝宝')
-        }}>
-        click
-      </button>
-      <Child name="huahua" />
-    </div>
-  )
+	const [name, setname] = useState<string>("hua")
+	return (
+		<div>
+			App--{name}
+			<button
+				onClick={() => {
+					setname("海绵宝宝")
+				}}
+			>
+				click
+			</button>
+			<Child name="huahua" />
+		</div>
+	)
 }
 // 使用接口约束props属性
 interface Iprop {
-  name: string
+	name: string
 }
 // function Child(props: Iprop) {
 //   return <div>child--{props.name}</div>
 // }
 //第二种写法,加上React.FC<接口约束>
-const Child: React.FC<Iprop> = (props) => {
-  return <div>child--{props.name}</div>
+const Child: React.FC<Iprop> = props => {
+	return <div>child--{props.name}</div>
 }
